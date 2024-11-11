19b13cd5f4d0006256e823db20ebfcb7c7d932da
mayur-0-0-7 <mayurjadhavak47117@gmail.com>
2024-11-11 20:21:04 +0530

    Add existing project filesnto Git

---
+++ script.js
@@ -0,0 +1,9 @@
+const food = document.querySelector('#food')
+const menu = document.querySelector('#menu')
+food.addEventListener('click',() => {
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');s
+    }
+})
